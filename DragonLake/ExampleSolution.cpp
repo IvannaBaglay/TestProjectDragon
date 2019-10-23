df7c91f70b60de90d3cf323d1233f4ed5ce1f87d
IvannaB <ivankamorkai.baglay@gmail.com>
2019-10-23 20:35:56 +0300

    Added the first version of the interface

--- DragonLake/ExampleSolution.cpp
+++ DragonLake/ExampleSolution.cpp
@@ -22,36 +22,75 @@ struct targetPoint
 
 struct ship
 {
+    std::vector<box> boxes;
 	float maxFuelWeight;
 	float maxCarryingWeight;
+    float resourcesConsumption;
+    struct maxCarryingCapacity
+    {
+        int half_x;
+        int half_y;
+        int half_z;
+    };
+
+
+
 };
 
-struct NameSurnamePathFinder : public IGalaxyPathFinder
+struct IvannaBaglayPathFinder : public IGalaxyPathFinder
 {
 private:
-	std::vector<box> boxes;
+	std::vector<box> boxes; // added only on base
 	std::vector<targetPoint> targetPoints;
 	ship myship;
 
 public:
 	virtual void FindSolution(const char* inputJasonFile, const char* outputFileName);
-	virtual const char* ShowCaptainName() { return "Name Surname"; }
+	virtual const char* ShowCaptainName() { return "Ivanna Baglay"; }
 };
 
-void NameSurnamePathFinder::FindSolution(const char* inputJasonFile, const char* outputFileName)
+void IvannaBaglayPathFinder::FindSolution(const char* inputJasonFile, const char* outputFileName)
 {
 	std::ifstream i(inputJasonFile);
 	
-	json j = json::parse(i, nullptr, false);
-
+	json j = json::parse(i); // create json with keys from file
+    json j_out;
 	// do some stuff
 
-	json j_out;
-	j_out["steps"] = json::array();
+    /*
+    {
+    LoadInformationFromJson(json);
+        {
+         myship = LoadInformationAboutShipFromJson(json["ship"]);
+         targetPoints = LoadInformationAboutTargetPointFromJson(json["targetPoints"]);
+         boxes = LoadInformationAboutBoxFromJson(json["boxes"]);
+        }   
+
+    }
+
+    while(!boxes.empty())
+    {
+    
+        shortestRoutes = FindShortestRoutes(targetPoints);
+            
+            for (int i = 0; i ! = shortestRoutes.size(); i++)
+            {
+                myship.PackBoxes(boxes, shortestRoutes[i]);
+                j_out["steps"] = json::array(DeliverBoxes());  // return string about shippedBoxes
+                                // like
+
+            }
+            
+    }
+
+    */
+
+	 
+	
 	// do some stuff
 
 	std::ofstream o(outputFileName);
-	o << std::setw(4) << j_out << std::endl;
+	o << std::setw(4) << j_out << std::endl; //Write solution in file
 }
 
 
--- DragonLake/json.h
+++ DragonLake/json.h
@@ -1,8 +1,19 @@
-#ifndef _DRAGONLAKE_JSON_H_ 
-#define _DRAGONLAKE_JSON_H_
+
+#ifndef _DRAGON_LAKE_JSON_H_ 
+#define _DRAGON_LAKE_JSON_H_
+
+#include <iostream>
+#include <array>
+
 
 class json {
+    json() {
 
+    }
+    json parse() {
+
+        return json();
+    }
 };
 
 #endif // _DRAGONLAKE_JSON_H_
